0506cff3e93e4628a3f363115b2f889a26e0f5f6
hikki <johan.chevet@hotmail.fr>
2022-01-13 16:15:49 +0100

    working

--- microshell.c
+++ microshell.c
@@ -4,13 +4,26 @@
 #include <stdio.h>
 #include <sys/wait.h>
 
+typedef enum e_type {
+	END,
+	PIPE,
+	BREAK
+}type;
+
 typedef struct s_cmd
 {
 	char **param;
+	int type;
 	int in;
 	int out;
+	struct s_cmd *prev;
 	struct s_cmd *next;
 }t_cmd;
+
+void exit_fatal(void)
+{
+	exit (EXIT_FAILURE);
+}
 
 int		ft_strlen(char *s)
 {
@@ -28,7 +41,7 @@ void	ft_putstr_fd(char *s, int fd)
 	write(fd, s, ft_strlen(s));
 }
 
-void	add_cmd(t_cmd **cmd, char **av, int i, int i_start)
+void	add_cmd(t_cmd **cmd, char **av, int i, int i_start, char t)
 {
 	t_cmd *new;
 	t_cmd *ptr;
@@ -38,6 +51,13 @@ void	add_cmd(t_cmd **cmd, char **av, int i, int i_start)
 	new->param = av + i_start; 
 	new->in = 0;
 	new->out = 1;
+	new->prev = NULL;
+	if (t == ';')
+		new->type = BREAK;
+	else if (t == '|')
+		new->type = PIPE;
+	else
+		new->type = END;
 	new->next = NULL;
 	ptr = *cmd;
 	if (!*cmd)
@@ -47,38 +67,8 @@ void	add_cmd(t_cmd **cmd, char **av, int i, int i_start)
 		while (ptr->next)
 			ptr = ptr->next;
 		ptr->next = new;
+		new->prev = ptr;
 	}
-}
-
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	char *ret;
-	int i = 0;
-
-	ret = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!ret)
-		return NULL;
-	if (s1)
-	{
-		while (s1[i])
-		{
-			ret[i] = s1[i];
-			i++;
-		}
-	}
-	int j = 0;
-	if (s2)
-	{
-		while (s2[j])
-		{
-			ret[i] = s2[j];
-			j++;
-			i++;
-		}
-	}
-	ret[i] = 0;
-	return ret;
 }
 
 void cd(t_cmd *cmd)
@@ -120,49 +110,55 @@ void exec_cmd(t_cmd *cmd, int pipein, char **env)
 	}
 }
 
-void	launch_process(t_cmd *cmd, char *sep, char **env)
+void	launch_process(t_cmd *cmd, char **env)
 {
 	int pipe_fd[2];
 	int pid;
-	int i = 0;
-
 	while (cmd)
 	{
-		if (!sep || !*sep || *sep == ';')
+		if (cmd->prev && cmd->prev->type == PIPE)
+			cmd->in = pipe_fd[0];
+		if (cmd->type == PIPE)
 		{
-			pid = fork();
-			if (pid == 0)
-				exec_cmd(cmd, 0, env);
-			while (wait(NULL) != -1)
-				;
+			if (pipe(pipe_fd) == -1)
+					exit_fatal();
+			cmd->out = pipe_fd[1];
 		}
-		else
+		pid = fork();
+		if (pid == -1)
+			exit_fatal();
+		else if (pid == 0)
 		{
-			if (i != 0)
-			{
-				cmd->in = pipe_fd[0];
-			}
-			if (cmd->next)
-			{
-				if (pipe(pipe_fd) == -1)
-					return ;
-				cmd->out = pipe_fd[1];
-			}
-			pid = fork();
-			if (pid == 0)
-				exec_cmd(cmd, pipe_fd[0], env);
-			else if (pid == -1)
-				return ;
-			if (cmd->next)
-				close(pipe_fd[1]);
+			if (cmd->type == PIPE)
+				close(pipe_fd[0]);
 			if (cmd->in != 0)
+			{
+				if (dup2(cmd->in, 0) == -1)
+					exit_fatal();
 				close(cmd->in);
+			}
+			if (cmd->out != 1)
+			{
+				if (dup2(cmd->out, 1) == -1)
+					exit_fatal();
+				close(cmd->out);
+			}
+			if (strcmp(cmd->param[0], "cd") == 0)
+				cd(cmd);
+			else if (execve(cmd->param[0], cmd->param, env) == -1)
+			{
+				//error blabla
+				exit(1);
+			}
 		}
-		if (sep && *sep == ';')
-			i = 0;
-		sep++;
+		if (cmd->type == PIPE)
+			close(pipe_fd[1]);
+		if (cmd->in != 0)
+			close(cmd->in);
+		if (cmd->type == BREAK)
+			while (waitpid(0, NULL, 0) != -1)
+				;
 		cmd = cmd->next;
-		i++;
 	}
 	while (wait(NULL) != -1)
 		;
@@ -173,10 +169,7 @@ int main(int ac, char **av, char **env)
 	t_cmd	*cmd;
 	int		i = 0;
 	int		i_start = 0;
-	char	*sep;
-	char	*tmp;
 
-	sep = NULL;
 	cmd = NULL;
 	av++;
 
@@ -184,23 +177,17 @@ int main(int ac, char **av, char **env)
 	{
 		while (av[i])
 		{
-			if (av[i][0] == '|' || av[i][0] == ';')
+			if ((av[i][0] == '|' && !av[i][1]) || (av[i][0] == ';' && !av[i][1]))
 			{
-				tmp = sep;
-				sep = ft_strjoin(sep, av[i]);
-				if (!sep)
-					return (1);
-				if (tmp)
-					free(tmp);
-				add_cmd(&cmd, av, i, i_start);
-				i_start = i + 1;
+				if (!av[i + 1] || av[i][0] == ';')
+					exit(1);
+				add_cmd(&cmd, av, i, i_start, av[i][0]);
+				i_start = i + 1;	
 			}
 			i++;
 		}
-		add_cmd(&cmd, av, i, i_start);
-		if (strcmp(*cmd->param, "cd") == 0)	
-			cd(cmd);
-		else
-			launch_process(cmd, sep, env);
+		add_cmd(&cmd, av, i, i_start, 0);
+		launch_process(cmd, env);
+		
 	}
 }
